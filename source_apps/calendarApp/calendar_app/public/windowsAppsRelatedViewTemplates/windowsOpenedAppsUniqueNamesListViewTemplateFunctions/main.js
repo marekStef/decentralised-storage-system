e8f7835d205f5b5ebaecd811e7f4a176fa9c0b18
stefanm4 <stefanecmarek@icloud.com>
2024-03-30 22:29:58 +0100

    added correct view template source code for getting windows opened apps data

--- source_apps/calendarApp/calendar_app/public/windowsAppsRelatedViewTemplates/windowsOpenedAppsUniqueNamesListViewTemplateFunctions/main.js
+++ source_apps/calendarApp/calendar_app/public/windowsAppsRelatedViewTemplates/windowsOpenedAppsUniqueNamesListViewTemplateFunctions/main.js
@@ -46,18 +46,156 @@ const getAllWindowsActivityTrackerEvents = (endpoint, tokenForEvents) => {
     }) 
 }
 
-function getUniqueExeNames(events) {
-    const uniqueExeNames = new Set();
+function getUniqueExeNames(dataEndpoint, tokenForWindowsActivityTrackerEvents) {
+    return new Promise((res, rej) => {
+        getAllWindowsActivityTrackerEvents(dataEndpoint, tokenForWindowsActivityTrackerEvents)
+        .then(response => {
+            const uniqueExeNames = new Set();
 
-    events.forEach(event => {
-        const exeName = event.payload?.exeName;
+            response.events.forEach(event => {
+                const exeName = event.payload?.exeName;
+        
+                if (exeName) {
+                    uniqueExeNames.add(exeName);
+                }
+            });
 
-        if (exeName) {
-            uniqueExeNames.add(exeName);
-        }
+            res({
+                code: 200,
+                uniqueExeNames: Array.from(uniqueExeNames),
+                response,
+                tokenForWindowsActivityTrackerEvents,
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res({
+                code: 500,
+                message: 'Something went wrong when requesting events'
+            })
+        });
+    })
+}
+
+function isDateWithinInterval(dateToCheckStr, startDateStr, endDateStr) {
+    const checkDate = new Date(dateToCheckStr).getTime();
+    const start = new Date(startDateStr).getTime();
+    const end = new Date(endDateStr).getTime();
+
+    return checkDate >= start && checkDate <= end;
+}
+
+// function getWindowsAppsEventsForSpecificInterval(dataEndpoint, tokenForWindowsActivityTrackerEvents, clientCustomData) {
+//     const intervalStartTime = clientCustomData.selectedWeek.startOfWeek;
+//     const intervalEndTime = clientCustomData.selectedWeek.endOfWeek;
+
+//     return new Promise((res, rej) => {
+//         getAllWindowsActivityTrackerEvents(dataEndpoint, tokenForWindowsActivityTrackerEvents)
+//             .then(response => {
+//                 const filteredWindowsApps = response.events.filter(event => isDateWithinInterval(event.metadata.createdDate, intervalStartTime, intervalEndTime));
+
+//                 res({
+//                     code: 200,
+//                     filteredWindowsApps,
+//                     response,
+//                 })
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//                 res({
+//                     code: 500,
+//                     message: 'Something went wrong when requesting events'
+//                 })
+//             });
+//     })
+// }
+
+// function getWindowsAppsEventsForSpecificInterval(dataEndpoint, tokenForWindowsActivityTrackerEvents, clientCustomData) {
+//     const intervalStartTime = new Date(clientCustomData.selectedWeek.startOfWeek);
+//     const intervalEndTime = new Date(clientCustomData.selectedWeek.endOfWeek);
+
+//     const days = Array.from({ length: 7 }, () => Array.from({ length: 24 }, () => []));
+
+//     return new Promise((res, rej) => {
+//         getAllWindowsActivityTrackerEvents(dataEndpoint, tokenForWindowsActivityTrackerEvents)
+//             .then(response => {
+//                 response.events.forEach(event => {
+//                     const eventDate = new Date(event.metadata.createdDate);
+//                     if (eventDate >= intervalStartTime && eventDate <= intervalEndTime) {
+//                         const dayOffset = Math.floor((eventDate - intervalStartTime) / (1000 * 60 * 60 * 24));
+//                         const hour = eventDate.getHours();
+//                         if (days[dayOffset] !== undefined && days[dayOffset][hour] !== undefined) {
+//                             days[dayOffset][hour].push(event);
+//                         }
+//                     }
+//                 });
+
+//                 res({
+//                     code: 200,
+//                     eventsByDayAndHour: days,
+//                     response,
+//                     clientCustomData: JSON.stringify(clientCustomData.appsWithAssignedCategories)
+//                 });
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//                 res({
+//                     code: 500,
+//                     message: 'Something went wrong when requesting events'
+//                 });
+//             });
+//     });
+// }
+
+function getWindowsAppsEventsForSpecificInterval(dataEndpoint, tokenForWindowsActivityTrackerEvents, clientCustomData) {
+    const intervalStartTime = new Date(clientCustomData.selectedWeek.startOfWeek);
+    const intervalEndTime = new Date(clientCustomData.selectedWeek.endOfWeek);
+    const appsWithAssignedCategories = clientCustomData.appsWithAssignedCategories;
+
+    const days = Array.from({ length: 7 }, () => Array.from({ length: 24 }, () => ({})));
+
+    return new Promise((res, rej) => {
+        getAllWindowsActivityTrackerEvents(dataEndpoint, tokenForWindowsActivityTrackerEvents)
+            .then(response => {
+                response.events.forEach(event => {
+                    const eventDate = new Date(event.metadata.createdDate);
+                    if (eventDate >= intervalStartTime && eventDate <= intervalEndTime) {
+                        const dayOffset = Math.floor((eventDate - intervalStartTime) / (1000 * 60 * 60 * 24));
+                        const hour = eventDate.getHours();
+                        const category = appsWithAssignedCategories[event.payload.exeName] || 'other';
+
+                        if (!days[dayOffset][hour][category]) {
+                            days[dayOffset][hour][category] = 0;
+                        }
+                        days[dayOffset][hour][category]++;
+                    }
+                });
+
+                const daysWithPercentages = days.map(day =>
+                    day.map(hour => {
+                        const totalEvents = Object.values(hour).reduce((acc, count) => acc + count, 0);
+                        return Object.keys(hour).reduce((acc, category) => {
+                            acc[category] = (hour[category] / totalEvents);
+                            return acc;
+                        }, {});
+                    })
+                );
+
+                res({
+                    code: 200,
+                    eventsByDayAndHour: daysWithPercentages,
+                    response,
+                    appsWithAssignedCategories
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                res({
+                    code: 500,
+                    message: 'Something went wrong when requesting events'
+                });
+            });
     });
-
-    return Array.from(uniqueExeNames);
 }
 
 const mainFunction = (parametersObject) => {
@@ -75,27 +213,27 @@ const mainFunction = (parametersObject) => {
             })
         }
 
-        getAllWindowsActivityTrackerEvents(dataEndpoint, tokenForWindowsActivityTrackerEvents)
-            .then(response => {
-                res({
-                    code: 200,
-                    uniqueExeNames: getUniqueExeNames(response.events),
-                    response,
-                    tokenForWindowsActivityTrackerEvents
+        if (clientCustomData.getUniqueApps == true) {
+            return getUniqueExeNames(dataEndpoint, tokenForWindowsActivityTrackerEvents)
+                .then(responseData => {
+                    res(responseData);
                 })
-                // res({
-                //     code: 200,
-                //     uniqueExeNames: getUniqueExeNames(response.events),
-                //     response,
-                //     tokenForWindowsActivityTrackerEvents
-                // })
-            })
-            .catch(err => {
-                res({
-                    code: 500,
-                    message: 'Something went wrong when requesting events'
+                .catch(errData => {
+                    res(errData);
                 })
-            });
+        }
+
+        if (clientCustomData.getAppsForSpecificDay == true) {
+            return getWindowsAppsEventsForSpecificInterval(dataEndpoint, tokenForWindowsActivityTrackerEvents, clientCustomData)
+                .then(responseData => {
+                    res(responseData);
+                })
+                .catch(errData => {
+                    res(errData);
+                })
+        }
+
+        res({ code: 400 });
     })
 }
 
