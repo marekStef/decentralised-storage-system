f0dbcd5f161841ba624ca730e6387cb19c4f1af0
stefanm4 <stefanecmarek@icloud.com>
2024-04-11 16:03:24 +0200

    removed currently irrelevant pieces of code

--- dataStorageSystem/backend/authService/src/utils/customErrors/dataViewStoreErrors.js
+++
@@ -1,8 +0,0 @@
-class DataViewStoreServerNotRunningError extends Error {
-    constructor(message = null) {
-      super(message);
-      this.name = 'DataViewStoreServerNotRunningError';
-    }
-}
-
-module.exports = { DataViewStoreServerNotRunningError }
