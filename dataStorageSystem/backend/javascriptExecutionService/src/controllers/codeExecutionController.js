461bc0269f43239a4fdfb11cc1382e0d09e35158
= <stefanecmarek@icloud.com>
2024-06-26 14:02:40 +0200

    completely removed isolated-vm from javascript execution service - running scrips in isolated environment safely is out of scope of this project. User of this component should be aware of the potential risks of running a 3rd party code

--- dataStorageSystem/backend/javascriptExecutionService/src/controllers/codeExecutionController.js
+++ dataStorageSystem/backend/javascriptExecutionService/src/controllers/codeExecutionController.js
@@ -1,4 +1,3 @@
-// const ivm = require('isolated-vm');
 // const fs = require('fs').promises;
 const path = require('path');
 
@@ -6,48 +5,6 @@ const httpStatusCodes = require("../constants/httpStatusCodes");
 const {getPathToGivenSourceCodeId} = require("./helpers/directory");
 
 const sourceCodeSpecificConstants = require('../constants/sourceCodeSpecific');
-
-
-// const runJsFileInIsolatedVm = async (filePath, args) => {
-//     const isolate = new ivm.Isolate({ memoryLimit: 128 });
-//     const context = await isolate.createContext();
-//     const jail = context.global;
-
-//     await jail.set('global', jail.derefInto());
-
-//     // Transfer the arguments to the isolated VM
-//     const argsTransferable = new ivm.ExternalCopy(args).copyInto({ release: true });
-//     await jail.set('args', argsTransferable);
-
-//     const fetchReference = new ivm.Reference(async (url, options) => {
-//         try {
-//             options = JSON.parse(options);
-//             console.log(url, options);
-//             console.log('---------');
-//             const response = await fetch(url, options);
-//             const text = await response.text(); // assuming json onlyn
-//             console.log(text);
-//             return new ivm.ExternalCopy(text).copyInto();
-//         } catch (error) {
-//             console.error('Fetch error:', error);
-//             throw new ivm.Reference(error);
-//         }
-//     });
-//     await jail.set('fetch', fetchReference);
-
-//     const code = await fs.readFile(filePath, 'utf8');
-//     try {
-//         const script = await isolate.compileScript(code);
-//         const result = await script.run(context);
-
-//         console.log(`Successfully ran ${path.basename(filePath)}, with return value:`, result);
-//         return result;
-//     } catch (error) {
-//         console.error(`Error running ${path.basename(filePath)}:`, error);
-//         return null;
-//     }
-// }
-
 
 const executeSourceCode = async (req, res) => {
     const { sourceCodeId } = req.params;
